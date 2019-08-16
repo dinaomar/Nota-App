939cce8e95d773fe21ee01ce60626412e2d88770
dina <dina.elcg@gmail.com>
2019-08-16 12:47:10 -0700

    koin dependency added

--- app/src/main/java/dina/nota/app/view/MainActivity.java
+++ app/src/main/java/dina/nota/app/view/MainActivity.java
@@ -5,17 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.nota.R;
-import android.arch.lifecycle.ViewModelProviders;
-import dina.nota.app.viewmodel.NoteViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
-    NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
     }
 }
